4a7b130dc26bacacd163481791b54cc1cce1df20
memoer <hanjn2842@naver.com>
2021-04-24 04:26:02 +0900

    chore/modify husky, git-checkout.sh

--- git-checkout-b.sh
+++ git-checkout-b.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
-read -n1 -p "Select Branch Name( [1]feat [2]fix [3]docs [4]refactor [5]test [6]chore ) >> " branchName
+read -n1 -p "Select Branch Name( [1]feat [2]fix [3]refactor [4]test [5]chore ) >> " branchName
 echo ""
 case ${branchName} in
   1) branchName="feat" ;;
   2) branchName="fix" ;;
-  3) branchName="docs" ;;
   4) branchName="refactor" ;;
   5) branchName="test" ;;
   6) branchName="chore" ;;
   *)
-    echo "first argument must be one of feat, fix, docs, refactor, test, chore"
+    echo "first argument must be one of feat, fix, refactor, test, chore"
     exit 1
     ;;
 esac
 
+desc=""
 read -n1 -p "Related Issue Number: " issueNumber
 echo ""
-if ! [[ ${issueNumber} =~ ^[0-9]+$ ]]; then
-  echo "related issue number must be number type"
-  exit 1
+if [[ ${issueNumber} =~ ^[0-9]+$ ]]; then
+  desc="/#${issueNumber}"
 fi
+  
+
 
 read -p "content: " content
 if ! [ -z "$content" ]; then
-  content="/${content}"
+  desc="${desc}/${content}"
 fi
 
-git checkout -b ${branchName}/\#${issueNumber}${content}
+git checkout -b ${branchName}${desc}
