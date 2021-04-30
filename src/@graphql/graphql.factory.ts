748ddbbfc953cdc8d87352d31ad589b1a14b8d13
memoer <hanjn2842@naver.com>
2021-05-01 05:19:01 +0900

    refactor(#1): graphql.factory.ts

--- src/@graphql/graphql.factory.ts
+++ src/@graphql/graphql.factory.ts
@@ -6,23 +6,20 @@ import isEnv from '~/_lib/isEnv';
 
 export default async (
   ac: ConfigType<typeof appConfig>,
-): Promise<GqlModuleOptions> => {
-  const options: GqlModuleOptions = {
-    playground: !isEnv('prod'),
-    debug: !isEnv('prod'),
-    // throttler 사용하려면 res까지 리턴해야 한다.
-    context: ({ req, res }) => ({ req, res }),
-    installSubscriptionHandlers: true,
-    uploads: {
-      maxFileSize: 10000000, // 10 MB
-      maxFiles: 5,
-    },
-    introspection: isEnv('local') || isEnv('dev'),
-    autoSchemaFile: join(__dirname, '..', 'schema.gql'),
-    cors: {
-      origin: ac.CORS_ORIGIN,
-      credentials: true,
-    },
-  };
-  return options;
-};
+): Promise<GqlModuleOptions> => ({
+  playground: !isEnv('prod'),
+  debug: !isEnv('prod'),
+  // throttler 사용하려면 res까지 리턴해야 한다.
+  context: ({ req, res }) => ({ req, res }),
+  installSubscriptionHandlers: true,
+  uploads: {
+    maxFileSize: 10000000, // 10 MB
+    maxFiles: 5,
+  },
+  introspection: isEnv('local') || isEnv('dev'),
+  autoSchemaFile: join(__dirname, '..', 'schema.gql'),
+  cors: {
+    origin: ac.CORS_ORIGIN,
+    credentials: true,
+  },
+});
