6c36946d912be54085890c62f185714a3eae1512
memoer <hanjn2842@naver.com>
2021-05-26 23:46:22 +0900

    refactor(): remove lambda-layer folder

--- lambda-layer/node_modules/apollo-server-cache-redis/src/__tests__/RedisClusterCache.test.ts
+++
@@ -1,6 +0,0 @@
-jest.mock('ioredis');
-
-import { RedisClusterCache } from '../index';
-import { testKeyValueCache } from '../../../apollo-server-caching/src/__tests__/testsuite';
-
-testKeyValueCache(new RedisClusterCache([{ host: 'localhost' }]));
